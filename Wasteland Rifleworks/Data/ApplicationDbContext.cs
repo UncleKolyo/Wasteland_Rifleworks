fbdab60917eac6a26106ba34d2646fc5fdbb86f1
Nick Vas <nicksoft@abv.bg>
2023-07-10 18:23:54 +0300

    Add Initial
    It includes some basic front end

---
+++ Wasteland Rifleworks/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿namespace Wasteland_Rifleworks.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
