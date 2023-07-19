f95fb5ddcbbd70c11ab48c7666398f0e2787ae4d
Nick Vas <nicksoft@abv.bg>
2023-07-19 19:53:03 +0300

    work

---
+++ WastelandRifleworks.Web.ViewModels/Home/SubmitViewModel.cs
@@ -0,0 +1,34 @@
+﻿namespace WastelandRifleworks.Web.ViewModels.Home
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using WastelandRilfeworks.Data.Models;
+
+    public class SubmitViewModel
+    {
+        [Required]
+        [MaxLength()]
+        public string Name { get; set; } = null!;
+
+        [Required]
+
+        public string Description { get; set; } = null!;
+
+        [Required]
+        [Range(0,100)]
+        public int Complexity { get; set; }
+
+        [Required]
+        [Range(1,100)]
+
+        public int Rating { get; set; }
+
+        [Required]
+
+        public int TypeId { get; set; }
+
+        public ICollection<Image> Images { get; set; }
+
+        public IEnumerable<TagViewModel> Tags { get; set; } = new List<TagViewModel>();
+    }
+}
---
+++ WastelandRifleworks.Web.ViewModels/Home/TagViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WastelandRifleworks.Web.ViewModels.Home
+{   
+    public class TagViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
